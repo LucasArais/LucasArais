f33896b2c40e3d69503dc092488d7a5f061c15e0
Lucas Arais de Souza <126637487+LucasArais@users.noreply.github.com>
2025-04-10 01:16:49 -0300

    Update atualizar_readme.sh

--- atualizar_readme.sh
+++ atualizar_readme.sh
@@ -1,7 +1,21 @@
 #!/bin/bash
 
-# Pega o repositório com o commit mais recente (usuário LucasArais)
 REPO=$(curl -s "https://api.github.com/users/LucasArais/repos?sort=pushed&per_page=1" | jq -r '.[0].name')
 
-# Faz o replace dentro do README
+if [ -z "$REPO" ]; then
+  echo "Erro: não foi possível obter o nome do repositório."
+  exit 1
+fi
+
 sed -i "s/<!-- REPO_ATUAL -->.*<!-- FIM_REPO_ATUAL -->/<!-- REPO_ATUAL -->$REPO<!-- FIM_REPO_ATUAL -->/" README.md
+
+if git diff --quiet README.md; then
+  echo "Nenhuma alteração no README.md. Nada a commitar."
+  exit 0
+else
+  git config --global user.name "github-actions[bot]"
+  git config --global user.email "41898282+github-actions[bot]@users.noreply.github.com"
+  git add README.md
+  git commit -m "📝 Atualiza projeto atual para: $REPO"
+  git push
+fi
